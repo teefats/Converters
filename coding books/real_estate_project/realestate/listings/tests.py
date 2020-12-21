2da796ef6249c53da994435c7316a4b66533f621
teefats <tee_fats@yahoo.com>
2020-12-21 14:11:09 +0000

    Added tests

--- coding books/real_estate_project/realestate/listings/tests.py
+++ coding books/real_estate_project/realestate/listings/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
 
 # Create your tests here.
+class SimpleTests(SimpleTestCase):
+    def test_listing_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEquals(response.status_code, 200)
+    def test_search_page_status_code(self):
+        response = self.client.get('/listings/search/')
+        self.assertEquals(response.status_code, 200)
