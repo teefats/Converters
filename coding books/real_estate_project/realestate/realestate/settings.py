6225513d8c892bd62021edc5edf158cd3ee3bc3b
teefats <tee_fats@yahoo.com>
2020-12-21 15:37:45 +0000

    Connected postgres database

--- coding books/real_estate_project/realestate/realestate/settings.py
+++ coding books/real_estate_project/realestate/realestate/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'realestate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gltredb',
+        'USER':'postgres',
+        'PASSWORD': 'Tunde_3483', 
+        'HOST':'localhost'
+
     }
 }
 
