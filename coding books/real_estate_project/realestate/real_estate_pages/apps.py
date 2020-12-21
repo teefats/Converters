d43fb3d77fd7a62cb7a6ee358f88f3a0f602a412
teefats <tee_fats@yahoo.com>
2020-12-21 10:52:37 +0000

    Added static files

---
+++ coding books/real_estate_project/realestate/real_estate_pages/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RealEstatePagesConfig(AppConfig):
+    name = 'real_estate_pages'
---
+++ coding books/real_estate_project/realestate/real_estate_pages/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(request):
+    return HttpResponse('<h1>Gloria</h1>')
+
+def about(request):
+    return render(request, 'real_estate_pages/about.html')
+
+def index(request):
+    return render(request, 'real_estate_pages/index.html')
