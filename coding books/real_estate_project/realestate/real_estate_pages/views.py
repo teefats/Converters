c425a4332808b7fb99c2ea7622912efbeddaf376
teefats <tee_fats@yahoo.com>
2020-12-22 13:28:37 +0000

    Added dynamic content

--- coding books/real_estate_project/realestate/real_estate_pages/views.py
+++ coding books/real_estate_project/realestate/real_estate_pages/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from realtors.models import Realtor
+from listings.models import Listing
 # Create your views here.
 # def index(request):
 #     return HttpResponse('<h1>Gloria</h1>')
 
 def about(request):
-    return render(request, 'real_estate_pages/about.html')
+    realtors = Realtor.objects.all()
+    context = {'realtors': realtors}
+    return render(request, 'real_estate_pages/about.html', context)
 
 def index(request):
-    return render(request, 'real_estate_pages/index.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published = True)[:3]
+    context = {
+        'listings':listings
+    }
+
+    return render(request, 'real_estate_pages/index.html', context)
