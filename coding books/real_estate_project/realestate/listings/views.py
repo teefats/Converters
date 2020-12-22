faca5f017ce7ad0661f3cdab780a83fb1a8bc325
teefats <tee_fats@yahoo.com>
2020-12-22 16:30:03 +0000

    Issues with search

--- coding books/real_estate_project/realestate/listings/views.py
+++ coding books/real_estate_project/realestate/listings/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Listing
-from listings.choices import state_choices, bedroom_choices, price_choices
+from .choices import state_choices, bedroom_choices, price_choices
 
 # Create your views here.
 
@@ -27,10 +27,40 @@ def listing(request, listing_id):
 
 def search(request):
     
+    query_set_list = Listing.objects.order_by('-list_date')
+
+    #keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            query_set_list = query_set_list.filter(description__icontains=keywords)
+    #city
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            query_set_list = query_set_list.filter(city__iexact=city)
+    #state
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            query_set_list = query_set_list.filter(state__iexact=state)
+    
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            query_set_list = query_set_list.filter(bedrooms__lte=bedrooms)
+
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            query_set_list = query_set_list.filter(price__lte=price)
+
     context = {
       
-         'state_choices': state_choices,
+        'state_choices': state_choices,
         'bedroom_choices': bedroom_choices,
         'price_choices': price_choices,
+        'listings': query_set_list, 
+        'values':request.GET
     }
     return render(request, 'listings/search.html', context)
